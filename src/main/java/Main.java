142199f4fb76315cd7641c800b190a692f8ff86a
Kanika <kanika@macbookpro.lan>
2025-03-02 18:09:43 -0500

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,33 @@
  * Modified by kristofer on Jan 2024.
  */
 public class Main {
-
-    public static void main(String[] args ){
-        // create a new object of class Main
-
+ private String name;
+ public Main(String name) {
+     this.name = name;
+ }
+    // create a new object of class Main
         // call the compute() method on that new object
-    }
 
-    public void compute() {
+    public static void main(String[] args ) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter your name : ");
+            String name = scanner.nextLine().trim();
+        Main mainObject = new Main();
+        mainObject.compute();
+        }
+ public void compute() {
+            if(name.equalsIgnoreCase("alice") ||
+                    name.equalsIgnoreCase("bob"))
+            {
+                System.out.println("Hello, " + name + "!");
+
+                }
+ }
+                else static {
+                    System.out.println("You are neither Alice nor Bob");
+                }
+                }
+
         // Get input string of name from user
         
         // Create conditional to check name against Alice and Bob
@@ -19,5 +38,3 @@ public class Main {
         
         // Print "You are neither Alice nor Bob." otherwise
 
-    }
-}
