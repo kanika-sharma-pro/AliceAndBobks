a09dc96bb8f6fbdadb649edd6e9c31c9898cab70
Kanika <kanika@macbookpro.lan>
2025-03-06 22:40:06 -0500

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,39 +2,41 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
+import java.util.Scanner;
 public class Main {
- private String name;
- public Main(String name) {
-     this.name = name;
- }
-    // create a new object of class Main
+
+    public static void main(String[] args) {
+
+        // create a new object of class Main
+        String name;
         // call the compute() method on that new object
+        compute();
+    }
 
-    public static void main(String[] args ) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter your name : ");
-            String name = scanner.nextLine().trim();
-        Main mainObject = new Main();
-        mainObject.compute();
+
+    public void compute() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = in.nextLine();
+        // Get input string of name from user
+
+        // Create conditional to check name against Alice and Bob
+
+        if (name.equalsIgnoreCase("alice") ||
+                name.equalsIgnoreCase("bob")) {
+            System.out.println("Hello, " + name + "!");
+
+        } else {
+            System.out.println("You are neither Alice nor Bob");
         }
- public void compute() {
-            if(name.equalsIgnoreCase("alice") ||
-                    name.equalsIgnoreCase("bob"))
-            {
-                System.out.println("Hello, " + name + "!");
-
-                }
- }
-                else static {
-                    System.out.println("You are neither Alice nor Bob");
-                }
-                }
+    }
+}
 
         // Get input string of name from user
-        
+
         // Create conditional to check name against Alice and Bob
 
         // Print greeting to screen if Alice or Bob are true
-        
+
         // Print "You are neither Alice nor Bob." otherwise
 
